3c0102addeb1b362e04a750fd20f4801a730b051
mhartington <mikehartington@gmail.com>
2017-12-19 11:26:24 -0500

    progress

--- packages/core/src/global/test/config-controller.spec.ts
+++ packages/core/src/global/test/config-controller.spec.ts
@@ -1,8 +1,11 @@
 import { createConfigController } from '../config-controller';
 
-// describe('Config', () => {
-//   it('should get a value from the config', () => {
-//     const config = createConfigController({ name: 'Doc Brown' }, null);
-//     expect(config.get('name').toEqual('Doc Brown'));
-//   });
-// });
+describe('Config', () => {
+  it('should get a value from the config', () => {
+    // const blah = 'Doc Brown';
+    // expect(blah).toEqual('Doc Brown');
+    const config = createConfigController({ name: 'Doc Brown' }, null);
+    console.log(config)
+    expect(config.get('name')).toEqual('Doc Brown');
+  });
+});
