d1a107eb4b3c2e4c7ed268488b33da97891a287c
mhartington <mikehartington@gmail.com>
2017-12-19 16:47:25 -0500

    more test

--- packages/core/src/global/test/config-controller.spec.ts
+++ packages/core/src/global/test/config-controller.spec.ts
@@ -2,10 +2,7 @@ import { createConfigController } from '../config-controller';
 
 describe('Config', () => {
   it('should get a value from the config', () => {
-    // const blah = 'Doc Brown';
-    // expect(blah).toEqual('Doc Brown');
-    const config = createConfigController({ name: 'Doc Brown' }, null);
-    console.log(config)
+    let config = createConfigController({ name: 'Doc Brown' }, null);
     expect(config.get('name')).toEqual('Doc Brown');
   });
 });
